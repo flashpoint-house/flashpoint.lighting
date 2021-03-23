4c5ffb6fe069c2a5081d912b62d47e335897704b
Donald Guy <donald@smartlogic.io>
2021-03-23 14:47:45 -0400

    master -> canon

--- push-site.sh
+++ push-site.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"
 yarn run build
 git add build
 git commit -m "Push site"
-git push origin `git subtree split --prefix build master`:gh-pages --force
+git push origin `git subtree split --prefix build canon`:gh-pages --force
 git rm -r ./build
 git reset HEAD@{1}
